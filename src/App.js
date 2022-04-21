6a521f83e0d0fef40a75a8492b4cad63f3e8bba6
Pandeyjiii8250 <rishabhpandey8250@gmail.com>
2022-04-22 02:56:40 +0530

    Basic login completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import "./App.css";
+
+import TextField from "@mui/material/TextField";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
 
 function App() {
+  const [name, setName] = useState('')
+  const [pass, setPass] = useState('')
+
+  const handleSubmit = () => {
+    //send data to back end 
+    //get if user is new or old and save it in higher state
+    setName('');
+    setPass('');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="container" sx={{display: 'flex'}}>
+      <div>
+        <div className="form-ctn">
+          <h1>Login Form</h1>
+          <TextField id="outlined-basic" label="Username" variant="outlined" value={name}/>
+          <TextField id="outlined-basic" label="Password" variant="outlined" value={pass}/>
+          <Button variant="contained" onClick={handleSubmit}>Submit</Button>
+        </div>
+      </div>
+    </Container>
   );
 }
 
