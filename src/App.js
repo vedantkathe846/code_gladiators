d07c8dae3308dfeba323c10e60e01d0cbb3b19d0
Pandeyjiii8250 <rishabhpandey8250@gmail.com>
2022-04-22 03:22:56 +0530

    basaic route dome

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
-import React, {useState} from 'react';
+import React from 'react';
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import TextField from "@mui/material/TextField";
-import Container from "@mui/material/Container";
-import Button from "@mui/material/Button";
-
+import Login from './Component/Login';
+import MainPage from './Component/MainPage';
 function App() {
-  const [name, setName] = useState('')
-  const [pass, setPass] = useState('')
 
-  const handleSubmit = () => {
-    //send data to back end 
-    //get if user is new or old and save it in higher state
-    setName('');
-    setPass('');
-  }
   return (
-    <Container className="container" sx={{display: 'flex'}}>
-      <div>
-        <div className="form-ctn">
-          <h1>Login Form</h1>
-          <TextField id="outlined-basic" label="Username" variant="outlined" value={name}/>
-          <TextField id="outlined-basic" label="Password" variant="outlined" value={pass}/>
-          <Button variant="contained" onClick={handleSubmit}>Submit</Button>
-        </div>
-      </div>
-    </Container>
-  );
+    <Router>
+      <Routes>
+        <Route path="/somewhere/else" element={<MainPage />}/>
+        <Route path='/' element={<Login />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/Component/Login/index.js
@@ -0,0 +1,35 @@
+
+import React, {useState} from 'react';
+import {useNavigate} from 'react-router-dom'
+import "./Login.css";
+
+import TextField from "@mui/material/TextField";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+
+export default function Login() {
+    const [name, setName] = useState('')
+    const [pass, setPass] = useState('')
+    const navigate = useNavigate()
+  
+    const handleSubmit = () => {
+      //send data to back end 
+      //get if user is new or old and save it in higher state
+      setName('');
+      setPass('');
+      navigate('/somewhere/else')
+
+    }
+  return (
+    <Container className="container" sx={{display: 'flex'}}>
+      <div>
+        <div className="form-ctn">
+          <h1>Login Form</h1>
+          <TextField id="outlined-basic" label="Username" variant="outlined" value={name} onChange={(e) => setName(e.target.value)} />
+          <TextField id="outlined-basic" label="Password" variant="outlined" value={pass} onChange={(e) => setPass(e.target.value)} />
+          <Button variant="contained" onClick={handleSubmit}>Submit</Button>
+        </div>
+      </div>
+    </Container>
+  )
+}
