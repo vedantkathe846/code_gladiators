8721f61078944032065106cade2fd87185b5fc49
Pandeyjiii8250 <rishabhpandey8250@gmail.com>
2022-04-22 09:54:43 +0530

    second commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
-          <img src={logo} />
+          {/* <img src={logo} /> */}
         </div>
         <div className="gpt3__navbar-links_container">
           <p><a href="#home">Home</a></p>
@@ -20,10 +20,10 @@ const Navbar = () => {
           <p><a href="#blog">Contact US</a></p>
         </div>
       </div>
-      <div className="gpt3__navbar-sign">
+      {/* <div className="gpt3__navbar-sign">
         <p>Sign in</p>
         <button type="button">Sign up</button>
-      </div>
+      </div> */}
       <div className="gpt3__navbar-menu">
         {toggleMenu
           ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
@@ -37,10 +37,10 @@ const Navbar = () => {
             <p><a href="#features">Case Studies</a></p>
             <p><a href="#blog">Contact US</a></p>
           </div>
-          <div className="gpt3__navbar-menu_container-links-sign">
+          {/* <div className="gpt3__navbar-menu_container-links-sign">
             <p>Sign in</p>
             <button type="button">Sign up</button>
-          </div>
+          </div> */}
         </div>
         )}
       </div>
