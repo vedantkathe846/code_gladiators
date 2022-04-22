3adf2bbeeb81f0179fb94e5dbfe85d0f3c171fb1
Pandeyjiii8250 <rishabhpandey8250@gmail.com>
2022-04-22 07:05:57 +0530

    implemented

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React, {useState} from 'react';
-import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainPage from './components/MainPage';
+import Login from './components/Login';
 
-import Blog from './Component/Blog';
-import Login from './Component/Login';
-import MainPage from './Component/MainPage';
-function App() {
-  const [isNew, updateIsNew] = useState(false);
-  return (
-    <Router>
-      <Routes>
-        <Route exact path='/blog/:id' element={<Blog />}/>
-        <Route exact path="/blogs" element={<MainPage isNew={isNew}/>}/>
-        <Route path='/' element={<Login update={updateIsNew} />} />
-      </Routes>
-    </Router>
-  )
-}
+import './App.css';
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route exact path="/blogs" element={<MainPage />}/>
+      <Route exact path="/" element={<Login />}/>
+    </Routes>
+  </Router>
+);
 
 export default App;
