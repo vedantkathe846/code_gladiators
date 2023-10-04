7e903ab8c8f5e4c06124431de449891eff4d8af7
Pandeyjiii8250 <rishabhpandey8250@gmail.com>
2023-10-04 22:31:50 +0530

    testing things
    this is it.

--- src/components/MainPage/index.js
+++ src/components/MainPage/index.js
@@ -3,7 +3,7 @@ import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from '../../con
 import { CTA, Brand, Navbar } from '../../components';
 
 const dummyData = {
-    'sports': [{
+    'Sports': [{
       'head': 'Lorem ipsum dolor sit amet',
       'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     }, {
@@ -16,7 +16,7 @@ const dummyData = {
       'head': 'Lorem ipsum dolor sit amet',
       'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     }],
-    'politics': [{
+    'Politics': [{
       'head': 'Lorem ipsum dolor sit amet',
       'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     }, {
@@ -29,7 +29,7 @@ const dummyData = {
       'head': 'Lorem ipsum dolor sit amet',
       'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     }],
-    'something': [{
+    'Misc': [{
       'head': 'Lorem ipsum dolor sit amet',
       'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     }, {
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -11,7 +11,7 @@ const Blog = ({section, newsData}) => (
     </div>
     <div className="gpt3__blog-container">
       <div className="gpt3__blog-container_groupA">
-        <Article imgUrl={blog01} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl={blog01} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us explore how it is?" />
       </div>
       <div className="gpt3__blog-container_groupB">
         {newsData.map((data, index) => (
